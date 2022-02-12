fdcc8a794308f09274a856e445ff438acdfa6a69
Marcelo Andrade <marcelo@devsres.com>
2022-02-12 02:47:03 -0300

    Tenancy_id var

--- user.tf
+++ user.tf
@@ -1,11 +1,9 @@
-variable compartment_id {
+variable tenancy_id {
     type = string
-       
-    default = "ocid1.compartment.oc1..aaaaaaaaxi7bvkugay7zbkey7h6hqlvptbt7dyc34q27q5nsmb6wzv3u4giq"
 }
 
 resource "oci_identity_user" "kube_user" {
-    
+    compartment_id = var.tenancy_id    
     description = "Usuário descartável apenas para testar Terraform" 
     name = "kube_user" 
 
